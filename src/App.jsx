7d8cbf62345cc3fd428966f63d2ce764669dd8c7
Daniyar410 <ramziya.shakirova@gmail.com>
2024-10-29 16:57:56 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.scss'
+import Header from './componensts/Header/Header'
+import Hero from './componensts/Hero/Hero'
+import Catalog from './componensts/Catalog/Catalog'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Catalog/>
+    </>
+  )
+}
+
+export default App
